5a53eba10d3725b79ef93477048ee0c0e098a543
fatmagulozgun <117574559+fatmagulozgun@users.noreply.github.com>
2025-07-11 15:10:15 +0300

    Statistics page was set up, customers page was completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,10 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from '../src/pages/HomePage'
 import CardPage from './pages/CardPage'
 import BillPage from './pages/BillPage'
+import CustomerPage from './pages/CustomerPage'
+import StatisticPage from './pages/StatisticPage'
+
+
 
 function App() {
   return (
@@ -9,7 +13,8 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/cart" element={<CardPage />} />
       <Route path="/bills" element={<BillPage />} />
-
+      <Route path="/customers" element={<CustomerPage />} />
+      <Route path="/statistic" element={<StatisticPage />} />
     </Routes>
 
   )
